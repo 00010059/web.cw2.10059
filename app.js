67d9b4f2676a7a0dd01167e54adcd1534c1c8434
00008878 <wiut10059@gmail.com>
2021-04-03 15:12:20 +0500

    delete functionality

--- app.js
+++ app.js
@@ -85,6 +85,26 @@ app.get('/blogs/:id', (req, res) => {
 })
 
 
+//Delete
+
+app.get('/blogs/:id/delete', (req, res) => {
+    fs.readFile('./data/blogs.json', (err, data) => {
+        if (err) throw err
+
+        const id = req.params.id
+        
+        const blogs = JSON.parse(data)
+        const blog = blogs.findIndex((e) => e.id == id)
+        blogs.splice(blog, 1)
+
+        fs.writeFile('./data/blogs.json', JSON.stringify(blogs), err => {
+            if (err) throw err
+            res.redirect('/blogs?deleted=success')
+        })
+    })
+})
+
+
 
 //Api
 
